2f12296063720ef559b760121767687b9921d382
NehalSurti <nehalsurti9998@gmail.com>
2025-08-06 23:59:28 +0100

    updating

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -5,20 +5,21 @@ import { Badge } from "../ui/badge";
 
 export default function HeroSection() {
   return (
-    <section>
-      <div className="">
-        <div className="flex">
-          <div className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
-            <Badge className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200">
-              <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse"></Sparkles>
-              <p className="text-base text-rose-600">Powered by AI</p>
-            </Badge>
-          </div>
+    <section className="relative mx-auto flex flx-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 transition-all animate-in lg:px-12 max-w-7xl">
+      <div className="flex">
+        <div className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
+          <Badge
+            variant={"secondary"}
+            className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200"
+          >
+            <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse"></Sparkles>
+            <p className="text-base text-rose-600">Powered by AI</p>
+          </Badge>
         </div>
-        <h1>Transform PDFs into concise summaries</h1>
-        <h2>Get a beautiful summary reel of the document in seconds.</h2>
-        <Button>Try Sommaire</Button>
       </div>
+      <h1>Transform PDFs into concise summaries</h1>
+      <h2>Get a beautiful summary reel of the document in seconds.</h2>
+      <Button>Try Sommaire</Button>
     </section>
   );
 }
