ced809bd4430931800f0777d55f5795c81fe6bbe
NehalSurti <nehalsurti9998@gmail.com>
2025-08-14 23:54:19 +0100

    Updating

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,3 +1,17 @@
+import { cn } from "@/lib/utils";
+import { CheckIcon } from "lucide-react";
+import Link from "next/link";
+
+type PriceType = {
+  name: string;
+  price: number;
+  description: string;
+  items: string[];
+  id: string;
+  paymentLink: string;
+  priceId: string;
+};
+
 const plans = [
   {
     id: "basic",
@@ -28,8 +42,60 @@ const plans = [
   },
 ];
 
-const PricingCard = () => {
-  return <div>PricingCard</div>;
+const PricingCard = ({
+  id,
+  name,
+  price,
+  description,
+  items,
+  paymentLink,
+  priceId,
+}: PriceType) => {
+  return (
+    <div className="relative w-full max-w-lg">
+      <div
+        className={cn(
+          "relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 border-[1px] border-gray-500/20 rounded-2xl",
+          id === "pro" && "border-rose-500 gap-5 border-2"
+        )}
+      >
+        <div className="flex justify-between items-center gap-4">
+          <div>
+            <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
+            <p className="text-base-content/80 mt-2">{description}</p>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <p className="text-5xl tracking-tight font-extrabold">${price}</p>
+          <div className="flex flex-col justify-end mb-[4px]">
+            <p className="text-xs uppercase font-semibold">USD</p>
+            <p className="text-xs">/month</p>
+          </div>
+        </div>
+        <div className="space-y-2.5 leading-relaxed text-base flex-1">
+          {items.map((item, idx) => (
+            <li key={idx} className="flex items-center gap-2">
+              <CheckIcon></CheckIcon>
+              <span>{item}</span>
+            </li>
+          ))}
+        </div>
+        <div className="space-y-2 flex justify-center w-full">
+          <Link
+            href={paymentLink}
+            className={cn(
+              "w-full rounded-full flex items-center justify-center gap-2 bg-linear-to-r from-rose-800 to-rose-500 hover:from-rose-500 hover:to-rose-800 text-white border-2 py-2",
+              id === "pro"
+                ? "border-rose-900"
+                : "border-rose-100 from-rose-400 to-rose-500"
+            )}
+          >
+            Buy Now
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function PricingSection() {
