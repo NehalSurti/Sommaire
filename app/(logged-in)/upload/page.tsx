31f6c0d80197b7f6c68b1f73b2683f09bb4636c3
NehalSurti <nehalsurti9998@gmail.com>
2025-08-20 23:48:11 +0100

    Updating

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -8,9 +8,12 @@ export default function Page() {
     <section className="min-h-screen">
       <BgGradient></BgGradient>
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
-        <div>
-          <Badge>
-            <Sparkles></Sparkles>
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <Badge
+            variant={"secondary"}
+            className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors"
+          >
+            <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse"></Sparkles>
             <span>AI-Powered Content Creation</span>
           </Badge>
           <h1>Start Uploading your PDF's</h1>
