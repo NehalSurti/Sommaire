7e7c7b0ce70fc1c7bb7388404ea8eda70b600587
NehalSurti <nehalsurti9998@gmail.com>
2025-09-15 23:54:41 +0100

    Updating

--- app/(logged-in)/dashboard/page.tsx
+++ app/(logged-in)/dashboard/page.tsx
@@ -9,17 +9,31 @@ export default function DashboardPage() {
     <main className="min-h-screen">
       <BgGradient className="from-emerald-200 via-teal-200 to-cyan-200"></BgGradient>
       <div className="container mx-auto flex flex-col gap-4">
-        <h1>Your Summaries</h1>
-        <p>Transform your PDFs into concise, actionable insights</p>
-
-        <Button
-          variant={"link"}
-          className="bg-linear-to-r from-rose-500 to-rose-700 hover:from-rose-600 hover:to-rose-800 hover:scale-105 transition-all duration-300 hover:no-underline"
-        >
-          <Link href="/upload" className="flex items-center text-white">
-            <Plus className="w-5 h-5 mr-2"></Plus>New Summary
-          </Link>
-        </Button>
+        <div className="px-2 py-12 sm:py-24">
+          <div className="flex gap-4 mb-8 justify-between">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl font-bold tracking-tight bg-linear-to-r from-gray-600 to-gray-900 bg-clip-text text-transparent">
+                Your Summaries
+              </h1>
+              <p className="text-gray-600">
+                Transform your PDFs into concise, actionable insights
+              </p>
+            </div>
+            <Button
+              variant={"link"}
+              className="bg-linear-to-r from-rose-500 to-rose-700 hover:from-rose-600 hover:to-rose-800 hover:scale-105 transition-all duration-300 hover:no-underline"
+            >
+              <Link href="/upload" className="flex items-center text-white">
+                <Plus className="w-5 h-5 mr-2"></Plus>New Summary
+              </Link>
+            </Button>
+          </div>
+          <div className="">
+            <p className="text-sm">
+              You've reached the limit of 5 uploads on the basic plan.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
