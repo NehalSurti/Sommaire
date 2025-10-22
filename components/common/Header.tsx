76df0838e8c57f3725e8f4a8cfaf48b1627d98d9
NehalSurti <nehalsurti9998@gmail.com>
2025-10-22 23:35:10 +0100

    Update Header

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,6 +3,7 @@ import { FileText } from "lucide-react";
 import { Button } from "../ui/button";
 import NavLink from "./Nav-Link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import PlanBadge from "./plan-badge";
 
 export default function Header() {
   return (
@@ -26,7 +27,7 @@ export default function Header() {
         <SignedIn>
           <div className="flex gap-2 items-center">
             <NavLink href="/upload">Upload a PDF</NavLink>
-            <div>Pro</div>
+            <PlanBadge></PlanBadge>
             <SignedIn>
               <UserButton />
             </SignedIn>
---
+++ components/common/plan-badge.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import { getUserByEmail } from "@/actions/userActions";
+import { pricingPlans } from "@/utils/constants";
+import { Badge } from "../ui/badge";
+import { cn } from "@/lib/utils";
+import { Crown } from "lucide-react";
+
+export default async function PlanBadge() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const email = user?.emailAddresses[0]?.emailAddress;
+  if (!email)
+    return (
+      <Badge
+        variant="outline"
+        className={cn(
+          "ml-2 bg-linear-to-r from-amber-100 to-amber-200  border-amber-300 hidden lg:flex items-center flex-row",
+          "from-red-100 to-red-200 border-red-300"
+        )}
+      >
+        <Crown
+          className={cn("w-3 h-3 mr-1 text-amber-600", "text-red-600")}
+        ></Crown>
+        Buy a plan
+      </Badge>
+    );
+
+  const { success, data } = await getUserByEmail(email);
+  const priceId = success && data ? data.priceId : null;
+
+  const plan = pricingPlans.find((p) => p.priceId === priceId);
+  const planName = plan?.name || "Buy a plan";
+
+  return (
+    <Badge
+      variant="outline"
+      className={cn(
+        "ml-2 bg-linear-to-r from-amber-100 to-amber-200  border-amber-300 hidden lg:flex items-center flex-row",
+        !priceId && "from-red-100 to-red-200 border-red-300"
+      )}
+    >
+      <Crown
+        className={cn(
+          "w-3 h-3 mr-1 text-amber-600",
+          !priceId && "text-red-600"
+        )}
+      ></Crown>
+      {planName}
+    </Badge>
+  );
+}
