a803e6861947e5f47be2bade98e34d76c8f3cf84
NehalSurti <nehalsurti9998@gmail.com>
2025-11-05 12:28:21 +0000

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { ORIGIN_URL } from "@/utils/env-helper";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -16,6 +17,23 @@ export const metadata: Metadata = {
   title: "Sommaire - AI-Powered PDF Summarization",
   description:
     "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+  openGraph: {
+    title: "Sommaire - AI-Powered PDF Summarization",
+    description:
+      "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+    siteName: "Sommaire",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        alt: "Sommaire Open Graph Image",
+      },
+    ],
+    type: "website",
+  },
+  metadataBase: new URL(ORIGIN_URL),
+  alternates: {
+    canonical: ORIGIN_URL,
+  },
 };
 
 export default function RootLayout({
