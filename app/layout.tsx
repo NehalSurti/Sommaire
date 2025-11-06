2634e1d59998f5456cbcf45f15d1fb6e177aeb59
NehalSurti <nehalsurti9998@gmail.com>
2025-11-06 12:32:53 +0000

    Updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,28 +13,28 @@ const fontSans = FontSans({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata: Metadata = {
-  title: "Sommaire - AI-Powered PDF Summarization",
-  description:
-    "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
-  openGraph: {
-    title: "Sommaire - AI-Powered PDF Summarization",
-    description:
-      "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
-    siteName: "Sommaire",
-    images: [
-      {
-        url: "/opengraph-image.png",
-        alt: "Sommaire Open Graph Image",
-      },
-    ],
-    type: "website",
-  },
-  metadataBase: new URL(ORIGIN_URL),
-  alternates: {
-    canonical: ORIGIN_URL,
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Sommaire - AI-Powered PDF Summarization",
+//   description:
+//     "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+//   openGraph: {
+//     title: "Sommaire - AI-Powered PDF Summarization",
+//     description:
+//       "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+//     siteName: "Sommaire",
+//     images: [
+//       {
+//         url: "/opengraph-image.png",
+//         alt: "Sommaire Open Graph Image",
+//       },
+//     ],
+//     type: "website",
+//   },
+//   metadataBase: new URL(ORIGIN_URL),
+//   alternates: {
+//     canonical: ORIGIN_URL,
+//   },
+// };
 
 export default function RootLayout({
   children,
