026d308a65eddee236a2c83ab5c4b70683f24a6c
NehalSurti <nehalsurti9998@gmail.com>
2026-02-24 23:58:42 +0000

    Refactor metadata declaration and improve layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,28 +13,28 @@ const fontSans = FontSans({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-// export const metadata: Metadata = {
-//   title: "Sommaire - AI-Powered PDF Summarization",
-//   description:
-//     "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
-//   openGraph: {
-//     title: "Sommaire - AI-Powered PDF Summarization",
-//     description:
-//       "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
-//     siteName: "Sommaire",
-//     images: [
-//       {
-//         url: "/opengraph-image.png",
-//         alt: "Sommaire Open Graph Image",
-//       },
-//     ],
-//     type: "website",
-//   },
-//   metadataBase: new URL(ORIGIN_URL),
-//   alternates: {
-//     canonical: ORIGIN_URL,
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Sommaire - AI-Powered PDF Summarization",
+  description:
+    "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+  openGraph: {
+    title: "Sommaire - AI-Powered PDF Summarization",
+    description:
+      "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+    siteName: "Sommaire",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        alt: "Sommaire Open Graph Image",
+      },
+    ],
+    type: "website",
+  },
+  metadataBase: new URL(ORIGIN_URL),
+  alternates: {
+    canonical: ORIGIN_URL,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -42,17 +42,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${fontSans.variable} antialiased`}>
+    <html lang="en">
+      <body className={`${fontSans.variable} antialiased`}>
+        <ClerkProvider>
           <div className="relative flex  min-h-screen flex-col ">
             <Header></Header>
             <main className="flex-1">{children}</main>
             {/* <Footer></Footer> */}
           </div>
           <Toaster richColors position="top-right" />
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
