23b4f050649e1a5f31c5cd8dbead17d09a330d1e
NehalSurti <nehalsurti9998@gmail.com>
2026-02-25 00:05:37 +0000

    Update ORIGIN_URL to use production URL for deployment

--- utils/env-helper.ts
+++ utils/env-helper.ts
@@ -3,5 +3,5 @@ export const isDevelopment = process.env.NODE_ENV === "development";
 
 export const ORIGIN_URL = isDevelopment
   ? "http://localhost:3000"
-  : "";
+  : "https://sommaire-five.vercel.app/";
 
